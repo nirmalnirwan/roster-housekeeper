4aba6a9cf473d84424369811b5c4005ca93a1fc7
tndni <tndnirmalz@gmail.com>
2026-03-10 21:12:55 +1300

    feat: update database connection strings and add migrations folder

--- roster-api-app/roster-api-app/Data/ApplicationDbContextFactory.cs
+++ roster-api-app/roster-api-app/Data/ApplicationDbContextFactory.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace roster_api_app.Data;
 
@@ -8,7 +10,10 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Database=rosterdb;Username=yourusername;Password=yourpassword");
+
+        // design-time constant connection string; keep in sync with appsettings
+        var connectionString = "Host=ep-curly-mode-adolqx8b-pooler.c-2.us-east-1.aws.neon.tech; Database=rosterdb; Username=neondb_owner; Password=npg_quXSiVZj5RM2; SSL Mode=VerifyFull; Channel Binding=Require;";
+        optionsBuilder.UseNpgsql(connectionString);
 
         return new ApplicationDbContext(optionsBuilder.Options);
     }
